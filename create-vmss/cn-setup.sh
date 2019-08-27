f33b73dc977241195de3d0805148fb3396ea0b0f
Ravi Panchumarthy <ravi.panchumarthy@intel.com>
2019-08-27 11:21:12 -0700

    Update cn-setup.sh

--- create-vmss/cn-setup.sh
+++ create-vmss/cn-setup.sh
@@ -26,3 +26,25 @@ mount -a
 
 # Don't require password for HPC user sudo
 echo "$USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# Load modules, Install miniconda, intel-TF
+echo `eval whoami` >> /home/$USER/whoami.log
+
+/usr/bin/modulecmd bash load gcc-8.2.0
+/usr/bin/modulecmd bash load mpi/impi_2018.4.274
+
+INSTALL_PREFIX=/opt
+
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b -p ${INSTALL_PREFIX}/miniconda3
+rm -rf Miniconda3-latest-Linux-x86_64.sh
+
+conda_path="export PATH=${INSTALL_PREFIX}/miniconda3/bin:$PATH"
+export PATH=${INSTALL_PREFIX}/miniconda3/bin:$PATH
+
+conda create -y --name intel-tf-py36 -c intel python=3 pip 
+${INSTALL_PREFIX}/miniconda3/envs/intel-tf-py36/bin/pip install --no-cache-dir intel-tensorflow horovod
+
+su - $USER -c "${INSTALL_PREFIX}/miniconda3/bin/conda init bash"
+echo "module load gcc-8.2.0" >> /home/$USER/.bashrc    
+echo "module load mpi/impi_2018.4.274" >> /home/$USER/.bashrc  
