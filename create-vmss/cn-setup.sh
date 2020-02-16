6b7df50b5d1072d87c94a0682afeb0d1c285e521
Santi Adavani <adavanisanti@users.noreply.github.com>
2020-02-15 22:53:43 -0800

    Updated cn-setup.sh

--- create-vmss/cn-setup.sh
+++ create-vmss/cn-setup.sh
@@ -5,7 +5,11 @@ set -e
 
 HEADNODE=$1
 USER=$2
+scriptUri=$3
 
+githubUser=$(echo "$scriptUri" | cut -d'/' -f4)
+githubRepo=$(echo "$scriptUri" | cut -d'/' -f5)
+githubBranch=$(echo "$scriptUri" | cut -d'/' -f6)
 # Add script for generating hostfile
 
 sed -i 's/^ResourceDisk.MountPoint=\/mnt\/resource$/ResourceDisk.MountPoint=\/mnt\/local_resource/g' /etc/waagent.conf
@@ -30,10 +34,22 @@ echo "$USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 # Load modules, Install miniconda, intel-TF
 echo `eval whoami` >> /home/$USER/whoami.log
 
+cd /home/$USER
+git clone -b $githubBranch https://github.com/$githubUser/$githubRepo.git
+cd azhpc-templates/create-vmss/scripts/
+chmod +x slurm.conf.sh
+
+# Add slurmctld restart in rc.local only on master node
+cp rc.local /etc/
+chmod +x /etc/rc.local
+
+chown $USER:$USER /home/$USER/azhpc-templates/create-vmss/scripts/
+
+bash slurm.conf.sh >> /mnt/resource/slurm/slurm.conf
+cp /mnt/resource/slurm/slurm.conf /etc/slurm/
 
 echo "NodeName=`hostname -s`" >> /etc/slurm/gres.conf
 echo `python /home/$USER/azhpc-templates/create-vmss/scripts/generate_node_conf.py` >> /mnt/resource/slurm/cluster.conf
-cp /mnt/resource/slurm/slurm.conf /etc/slurm/
 
 # Restart munge slurmd
 # systemctl daemon-reload
